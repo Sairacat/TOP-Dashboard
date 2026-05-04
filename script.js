acbe3710f8ed8d58ee684e492fcc866170ab8ab2
Sairacat <201769jzy@gmail.com>
2026-05-04 16:01:40 +0800

    Finished the basic structure of the dashborad. Add some animation to the sidebar, whenever user click the hambergur button, sidebar will fold.

---
+++ script.js
@@ -0,0 +1,9 @@
+const layout = document.querySelector('.layout')
+const header = document.querySelector('.header')
+const mainContent = document.querySelector('.main-content')
+const sidebar = document.querySelector('.sidebar')
+const togglebtn = document.querySelector('.toggle-btn')
+
+togglebtn.addEventListener('click', function(e) {
+    layout.classList.toggle('collapse')
+})
