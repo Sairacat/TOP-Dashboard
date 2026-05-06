0890bd3abdaacc00ca0dcf58603fb1859a28b2b6
Sairacat <201769jzy@gmail.com>
2026-05-06 13:17:43 +0800

    Completed the header part. Using flexbox for the most of time in header. Add some hover style to the buttons and user avatars. One little annoying problem is that whene- ver sidebar collapses the flex items in the header follow it. which makes the overall layout in the header kind of ugly.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const header = document.querySelector('.header')
 const mainContent = document.querySelector('.main-content')
 const sidebar = document.querySelector('.sidebar')
 const togglebtn = document.querySelector('.toggle-btn')
+const firstLine = document.querySelector('.first-line')
 
 togglebtn.addEventListener('click', function(e) {
     layout.classList.toggle('collapse')
