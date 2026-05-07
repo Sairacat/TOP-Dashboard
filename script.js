c252792f4218fc95bd248b6087235faf5e1eb57e
Sairacat <201769jzy@gmail.com>
2026-05-07 14:40:06 +0800

    Added a theme toggle button to the header. By changing the className property of :root, and other custom properties within it, users can change the theme from light blue to lightgreen and vise versa. Using appearance: none; to fully rebu ilt the appearance of a checkbox.

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ const mainContent = document.querySelector('.main-content')
 const sidebar = document.querySelector('.sidebar')
 const togglebtn = document.querySelector('.toggle-btn')
 const firstLine = document.querySelector('.first-line')
+const themeToggle = document.querySelector('#theme-toggle')
 
 togglebtn.addEventListener('click', function(e) {
     layout.classList.toggle('collapse')
 })
+
+themeToggle.addEventListener('click', changeTheme)
+
+function changeTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'lightblue' ? 'lightgreen' : 'lightblue';
+    root.className = newTheme
+}
